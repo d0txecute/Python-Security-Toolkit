d318cef5d501cd34cd655b26026e8bbcb0b8c2e0
Lavender-exe <deathstrikerz123@gmail.com>
2023-04-03 00:45:18 +0400

    Update

--- Toolkit.py
+++ Toolkit.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3 
+import pyfiglet
+
+banner = pyfiglet.figlet_format("Python Security Toolkit", font = "larry3d"  )
+print (banner)
 
 print ("MAIN MENU".center(80, "—"))
 
@@ -12,6 +16,9 @@ print ("4. Subdomain Enumerator")
 
 print ("\nBrute Force\n")
 print ("5. Hash Cracker")
+
+# print ("\nUtility\n")
+
 
 selection = input("Select a Program Number: ")
 print("")
@@ -34,3 +41,11 @@ match selection:
     # Brute Force
     case "5":
         from Bruteforce import hashcracker
+
+    # Utility
+    case TypeError:
+        print ("""
+        Linux:      python3 Toolkit.py
+        Mac:        python3 Toolkit.py
+        Windows:    .\python.exe Toolkit.py
+        """)
