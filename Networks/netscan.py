2e51b564ecb85af741d85d1d2f7263a5bd49f953
Lavender-exe <deathstrikerz123@gmail.com>
2023-04-03 02:16:08 +0400

    Network Scanner

--- Networks/netscan.py
+++ Networks/netscan.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
 
+from scapy.all import *
+import argparse
+
 print ("Network Scanner".center(80, "—"))
 
-from scapy.all import *
+# Selections
+interface = input("Select Interface (eth0, wlan0, etc): ")
+ip_range = input("Enter IP Range (10.10.X.X/24): ")
 
-# Select Interface ex. eth0
-interface = f"{sys.argv[1]}"
-
-# Select IP Range ex. 10.10.X.X/24
-ip_range = f"{sys.argv[2]}"
+# MAC will be replaced with Ether.src
 broadcastMac = "ff:ff:ff:ff:ff:ff"
-packet = Ether(dst=broadcastMac)/ARP(pdst = ip_range)
 
-ans, unans = srp(packet, timeout =2, iface=interface, inter=0.1)
+packet = Ether(dst = broadcastMac) / ARP(pdst = ip_range)
 
-for send,receive in ans:
-       print (receive.sprintf(r"%Ether.src% - %ARP.psrc%"))
+ans, unans = srp (packet, timeout = 1, iface = interface, inter = 0.1)
+
+for send, receive in ans:
+    print (receive.sprintf(r"%Ether.src% - %ARP.psrc%"))
