07c61bad582cfeb15620eafeea5507825552b26c
HAM-MAW <hamoodmaawaliW@gmail.com>
2023-04-05 00:51:39 +0400

    Added switch case and passed method as variable to avoid repeating code

--- Bruteforce/hashcracker.py
+++ Bruteforce/hashcracker.py
@@ -4,56 +4,53 @@ from colorama import Fore
 
 # Goes back a directory then imports from the Config directory
 sys.path.append('..')
-from Config import colours
+#from Config import colours
 
-colours.HashCrackBanner()
+#colours.HashCrackBanner()
 
 wordlist_location = os.path.abspath('../Python-Security-Toolkit/Wordlists/passwords.txt')
 
-#Class containing hashing algorithms crackers functions
-class HashingAlgorithmsCracker:
-    def sha256_cracker(hash_input):
-        with open(wordlist_location, 'r') as file:
-            for line in file.readlines():
+def hash_cracker(hash_input, hashtype):
+    with open(wordlist_location, 'r') as file:
+        for line in file.readlines():
 
-                # Replace Sha256 with a hash type
-                hash_ob = hashlib.sha256(line.strip().encode())
-                hashed_pass = hash_ob.hexdigest()
-                if hashed_pass == hash_input:
-                    print(Fore.GREEN + '[+] Password Found: ' + line.strip())
-                    break
-            else:
-                print(Fore.RED + '[+] Password Not Found')
+            # Replace Sha256 with a hash type to avoid redu
+            hash_ob = hashtype(line.strip().encode())
+            hashed_pass = hash_ob.hexdigest()
+            if hashed_pass == hash_input:
+                print(Fore.GREEN + '\n[+] Password Found: ' + line.strip())
+                break
+        else:
+            print(Fore.RED + '\n[!] Password Not Found')
 
-    def sha1_cracker(hash_input):
-        with open(wordlist_location, 'r') as file:
-            for line in file.readlines():
+#Choose hashing algorithm and type in the hash to be cracked
+def set_hashing_algorithm():
+    hash_input = str(input('[+] Enter hash to be cracked: '))
+    hashtype = input(str("\n[1] SHA1\n[2] SHA224\n[3] SHA256\n[4] SHA384\n[5] SHA512\n[6] MD5\n[+] Enter Hash Type(number): "))
 
-                # Replace Sha256 with a hash type
-                hash_ob = hashlib.sha1(line.strip().encode())
-                hashed_pass = hash_ob.hexdigest()
-                if hashed_pass == hash_input:
-                    print(Fore.GREEN + '[+] Password Found: ' + line.strip())
-                    break
-            else:
-                print(Fore.RED + '[+] Password Not Found')
-
-#Choose hashing algorithm
-def set_hashing_algorithm(hash_input, hashtype):
-    if hashtype == 'sha256':
-        HashingAlgorithmsCracker.sha256_cracker(hash_input)
-    if hashtype == 'sha1':
-        HashingAlgorithmsCracker.sha1_cracker(hash_input)
+    match hashtype:
+        case '1':
+            hash_cracker(hash_input, hashlib.sha1)
+        case '2':
+            hash_cracker(hash_input, hashlib.sha224)
+        case '3':
+            hash_cracker(hash_input, hashlib.sha256)
+        case '4':
+            hash_cracker(hash_input, hashlib.sha384)
+        case '5':
+            hash_cracker(hash_input, hashlib.sha512)
+        case '6':
+            hash_cracker(hash_input, hashlib.md5)
+        case _:
+            print("\nIncorrect input.\nPlease enter a number.")
 
 while(True):
-    hash_input = str(input('[+] Enter hash to be cracked: '))
-    hashtype = input("[+] Enter Hash Type: ").lower()
-
-    set_hashing_algorithm(hash_input, hashtype)
+    set_hashing_algorithm()
     
-    decision = input(str(Fore.WHITE + '[+] Do you want to enter another hash? (y/n): ')).lower()
-    print('\n')
+    decision = input(str(Fore.RESET + '\n[+] Do you want to enter another hash? (y/n): ')).lower()
+    print('=' * 80)
     if decision == 'y':
         continue
     else:
         exit(0)
+
