bb99825643cbade5ebd4e7f48e739dbac16f636f
Lavender-exe <deathstrikerz123@gmail.com>
2023-04-04 16:32:44 +0400

    Updated Subdomain Fuzzer

--- Webapp/subdomain_enum.py
+++ Webapp/subdomain_enum.py
@@ -1,11 +1,38 @@
 #!/usr/bin/python3
-import os,sys,argparse
-from scapy.all import *
+import requests, sys, os
 
 # Goes back a directory then imports from the Config directory
 sys.path.append('..')
+from colorama import *
 from Config import colours
 
 colours.SubBanner()
 
+def domain_scan (domain_name, sub_names):
+    print ('Subdomains'.center(80, '-'))
 
+    for subdomain in sub_names:
+        url = f"http://{subdomain}.{domain_name}"
+
+        try: # Displays successful results
+            requests.get(url)
+            print (Fore.GREEN + f"[+] {url}")
+            print (Style.RESET_ALL)
+
+        # If Conection Error, skip to next subdomain
+        except requests.ConnectionError:
+            pass
+
+if __name__ == '__main__':
+
+    # Wordlist Path
+    wordlist = os.path.abspath('../Wordlists/subdomain.txt')
+
+    dom_name = input (Fore.BLUE + "[i] Enter Domain: ")
+    print ('\n')
+
+    with open (wordlist, 'r') as file:
+        name = file.read()
+        sub_dom = name.splitlines()
+
+    domain_scan(dom_name, sub_dom)
