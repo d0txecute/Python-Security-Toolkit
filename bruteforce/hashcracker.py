0b2d70102c678131e4c4b31c958511874bdd1272
Lav <32195948+Lavender-exe@users.noreply.github.com>
2023-04-25 17:04:06 +0400

    Add files via upload

---
+++ bruteforce/hashcracker.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+import hashlib
+from config.colours import *
+from config.commands import *
+clear()
+HashCrackBanner()
+
+def hash_cracker(hash_input, hashtype):
+    with open("wordlists/passwords.txt", 'r') as file:
+        for line in file.readlines():
+
+            hash_obj = hashtype(line.strip().encode())
+            hashed_pass = hash_obj.hexdigest()
+            if hashed_pass == hash_input:
+                success("Password Found: ")
+                break
+        else:
+            fail('Password Not Found')
+
+
+# Choose hashing algorithm and type in the hash to be cracked
+def set_hashing_algorithm():
+    hash_input = input(str(f'{Fore.BLUE}[i] Enter hash to be cracked: '))
+    hashtype = input(
+        str(Fore.BLUE + "\n[1] SHA1\n[2] SHA224\n[3] SHA256\n[4] SHA384\n[5] SHA512\n[6] MD5\n[+] Enter Hash Type(number): "))
+    print(Style.RESET_ALL)
+    match hashtype:
+        case '1':
+            hash_cracker(hash_input, hashlib.sha1)
+        case '2':
+            hash_cracker(hash_input, hashlib.sha224)
+        case '3':
+            hash_cracker(hash_input, hashlib.sha256)
+        case '4':
+            hash_cracker(hash_input, hashlib.sha384)
+        case '5':
+            hash_cracker(hash_input, hashlib.sha512)
+        case '6':
+            hash_cracker(hash_input, hashlib.md5)
+        case _:
+            print(Fore.RED + "\n[-] Incorrect input.\nPlease enter a number (1 - 6).")
+
+
+while True:
+    set_hashing_algorithm()
+
+    decision = input(str(Fore.BLUE + '\n[i] Do you want to enter another hash? (y/n): ')).lower()
+    print('=' * 80)
+    if decision == 'y':
+        continue
+    else:
+        exit(0)
---
+++ networks/portscan.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import os, sys, argparse
+from scapy.all import *
+from config.colours import *
+
+PortScanBanner()
+
+
